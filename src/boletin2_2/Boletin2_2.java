746739aed7986b0fe7989667c4315c5736843d75
Brei <Brei@Brei-PC>
2015-10-05 11:03:16 +0200

    area de un cadrado

---
+++ src/boletin2_2/Boletin2_2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin2_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Brei
+ */
+public class Boletin2_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float lado;
+        System.out.println("Introduce un lado");
+        Scanner ladoCuadrado = new Scanner(System.in);
+        lado = ladoCuadrado.nextFloat();
+        System.out.println("Área total cuadrado " +lado*lado);
+    }
+    
+}
